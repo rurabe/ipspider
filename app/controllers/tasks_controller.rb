6206143ec4625c1096a2c4e89249377b96f35267
Ryan Urabe <rurabe@gmailcom>
2013-04-16 23:39:56 -0700

    tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   def create
-    
+    heroku = Heroku::API.new
+    heroku.post_ps('ipspider',"rake spider:crawl[#{params[:url]},#{params[:channel]}]")
+    render :nothing => true
   end
 end
